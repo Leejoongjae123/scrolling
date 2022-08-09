d6b185b70d050e6dcfd9fe1b7b0607aa7fb5d046
JJ <ljj3347@naver.com>
2022-08-09 14:23:56 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [position, setPosition] = useState(0);
+  function onScroll() {
+    setPosition(window.scrollY);
+  }
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);  
+  console.log(position)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div 
+      className="bg bg1"
+      style={{backgroundPositionY:position/2}}
+      >
+        <div>Welcome</div>
+      </div>
+      <div 
+      className="bg bg2" 
+      style={{backgroundPositionY:position/-3}}
+      >
+        <div>Happy New Year</div>
+      </div>
+      <p 
+      className="desc"
+      style={{transform:`translateX(${-position}px)`}}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+      <p 
+      className="desc2"
+      style={{transform:`translateX(${position}px)`}}
+      >
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p 
+      className="desc3"
+      style={{opacity:(position-1300)/500}}
+      >
+        Duis aute irure dolor
+      </p>
+      <p
+      className="desc3"
+      style={{opacity:(position-1300)/500}}
+      >
+        Lorem ipsum dolor sit amet
+      </p>
+      <p
+      className="desc3"
+      style={{opacity:(position-1300)/500}}
+      >
+        Excepteur sint occaecat
+      </p>
+      <p
+      className="desc3"
+      style={{opacity:(position-1300)/800}}
+      >
+        sunt in culpa qui officia deserunt
+      </p>
+      <img src='' className="item" alt=""/>
+      <img src='' className="item item_snow" alt=""/>
     </div>
   );
 }
